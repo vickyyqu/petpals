841130abb4aa5641ff68a9e4e9532a14ebc28bd6
vickyyqu <112297073+vickyyqu@users.noreply.github.com>
2022-10-28 14:32:49 +0800

    edited profile listing page

--- g3t4-petpals/src/views/profilelisting.vue
+++ g3t4-petpals/src/views/profilelisting.vue
@@ -1,239 +1,220 @@
 <template>
-    <navbar></navbar>
-    <div class="content" style="padding-bottom: 50px;">
-        <div class="inner">
-        <div class="top-div">
-            <h6>Add Your Pet Service</h6>
-        </div>
 
-        <div class="row">
-            <div class="input-container">
-            <div class="row">
-            <div>Services provided:</div> 
-            </div>
-            <div class="input-row">
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Sitter" id="petSitter" v-model="services">
-                    <label class="form-check-label ps-2" for="petSitter">
-                    <img src="../img/sitter.png" style="width:70px; height:60px;">
-                        Pet Sitter
-                    </label>
+    <div class="container-fluid">
+        <navbar></navbar>
+        <div class="row pt-5">
+            <div class="col-1 sides"></div>
+
+            <div class="col-1"></div>
+
+            <div class="col-8 pt-5 text-center">
+                <h3 class="text-center mt-4 mb-2 pb-3">Add Your Pet Service Listing</h3>
+
+                <div class="row w-75 mx-auto">
+
+                    <div class="col-md-6 mt-3">
+                        <label for="yearsofexp">Display Name:</label>
+                        <input type="text" class="form-control" id="displayName" v-model="displayName">
+                    </div>
+
+                    <div class="col-md-6 mt-3">
+                        <label for="rate">Your Contact Number:</label>
+                        <input type="text" class="form-control" min="0" max="1000" v-model="contactNumber">
+                    </div>
+
+                    <div class="col-md-12 my-3">
+                        <label for="rate">Your Email:</label>
+                        <input type="text" class="form-control" v-model="email">
+                    </div>
+
                 </div>
 
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Walker" id="petWalker" v-model="services">
-                    <label class="form-check-label ps-2" for="petWalker">
-                    <img src="../img/dogwalker.jpeg" style="width:70px; height:60px;">
-                        Pet Walker
-                    </label>
+                <h4 class="my-3">Your Provided Services:</h4>
+
+                <div class="row">
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Sitter" id="petSitter"
+                            v-model="services">
+                        <label class="form-check-label ps-4" for="petSitter">
+                            <img src="../img/png/sitter.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Sitter</p>
+                        </label>
+                    </div>
+
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Walker" id="petWalker"
+                            v-model="services">
+                        <label class="form-check-label ps-4" for="petWalker">
+                            <img src="../img/png/walker.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Walker</p>
+                        </label>
+                    </div>
+
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Groomer" id="petGroomer"
+                            v-model="services">
+                        <label class="form-check-label ps-4" for="petGroomer">
+                            <img src="../img/png/groomer.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Groomer</p>
+                        </label>
+                    </div>
+
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Hotel" id="petHotel"
+                            v-model="services">
+                        <label class="form-check-label ps-4 pt-3" for="petHotel">
+                            <img src="../img/png/hotel.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Hotel</p>
+                        </label>
+                    </div>
+
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Trainer" id="petTrainer"
+                            v-model="services">
+                        <label class="form-check-label ps-4 pt-3" for="petTrainer">
+                            <img src="../img/png/trainer.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Trainer</p>
+                        </label>
+                    </div>
+
+                    <div class="col-sm-4 form-check d-flex align-items-start justify-content-center">
+                        <input class="form-check-input" type="checkbox" value="Pet Mover" id="petMover"
+                            v-model="services">
+                        <label class="form-check-label ps-4 pt-3" for="petMover">
+                            <img src="../img/png/catincar.png" style="width:60px; height:60px;"
+                                class="rounded rounded-circle mb-2">
+                            <p>Pet Mover</p>
+                        </label>
+                    </div>
+
                 </div>
 
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Groomer" id="petGroomer" v-model="services">
-                    <label class="form-check-label ps-2" for="petGroomer">
-                    <img src="../img/groomer.jpeg" style="width:70px; height:60px;">
-                        Pet Groomer
-                    </label>
+                <h4 class="mt-3">Services Description:</h4>
+                <textarea class="form-control mt-2 w-75 mx-auto" id="description" rows="4" cols="50"
+                    placeholder="Provide us with an overview of the services you offer." v-model="description"></textarea>
+
+                <h4 class="mt-3">More Details:</h4>
+
+                <div class="row w-75 mx-auto">
+                    <div class="col-md-6 form-group mt-3">
+                        <label for="petTypes">Pet Size You Accept:</label>
+                        <select name="number" class="form-control" id="size" v-model="sizeofpet"
+                            placeholder="Choose...">
+                            <option value="1kg">1-5kg</option>
+                            <option value="5kg">5-10kg</option>
+                            <option value="10kg">10-20kg</option>
+                            <option value="20kg">20-40kg</option>
+                            <option value="40kg">40+kg</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-6 mt-3">
+                        <label for="yearsofexp">Years of Experience:</label>
+                        <input type="number" class="form-control" id="yearsofexp" min="0" max="50">
+                    </div>
+
+                    <div class="col-md-12 mt-3">
+                        <label for="rate">Your Rate in SGD (Per Hour):</label>
+                        <input type="number" class="form-control" min="0" max="1000" v-model="rate">
+                    </div>
+
                 </div>
-            </div>
 
-            <div class="input-row">
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Hotel" id="petHotel" v-model="services">
-                    <label class="form-check-label ps-2" for="petHotel">
-                    <img src="../img/hotel.jpeg" style="width:70px; height:60px;">
-                        Pet Hotel
-                    </label>
-                </div>
-
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Trainer" id="petTrainer" v-model="services">
-                    <label class="form-check-label ps-2" for="petTrainer">
-                    <img src="../img/trainer.jpeg" style="width:70px; height:60px;">
-                        Pet Trainer 
-                    </label>
-                </div>
-
-                <div class="form-check-inline">
-                    <input class="form-check-input" type="checkbox" value="Pet Mover" id="petMover" v-model="services">
-                    <label class="form-check-label ps-2" for="petMover">
-                    <img src="../img/transport.webp" style="width:70px; height:60px;">
-                        Pet Mover &nbsp;&nbsp;&nbsp;&nbsp;
-                    </label>
-                </div>
-            </div>
-            </div>
-        </div>
-
-        <br>
-
-            <div class="row">
-            <div class="input-container">
-                <label>Service Description</label>
-                <textarea id="description" rows="3" cols="50"  placeholder="Give an overview of what services you offer." v-model="description"></textarea>
-            </div>
-            </div>
-
-            <!-- <div class="input-row">
-            <div class="input-container">
-                <label for="number">Number of pets you are willing to take in</label>
-                <input type="number" min="1" max="7" v-model="number">
-            </div>
-            </div> -->
-
-        <div class="input-container">
-            <label>Types of pets you accept</label>
-            <div>
-            <input class="form-check-input" type="checkbox" value="Dogs" id="dog" v-model="typeofpet">
-            <label class="form-check-label ps-2" for="dog">
-            <img src="../img/animallogo/doglogo.png" style="width:70px; height:60px;">
-                Dogs
-            </label>
-            </div>
-
-            <div>
-            <input class="form-check-input" type="checkbox" value="Cats" id="cat" v-model="typeofpet">
-            <label class="form-check-label ps-2" for="cat">
-            <img src="../img/animallogo/catlogo.png" style="width:70px; height:60px;">
-                Cats
-            </label>
-            </div>
-
-            <div>
-            <input class="form-check-input" type="checkbox" value="Rabbits" id="rabbit" v-model="typeofpet">
-            <label class="form-check-label ps-2" for="rabbit">
-            <img src="../img/animallogo/rabbitlogo.png" style="width:70px; height:60px;">
-                Rabbits
-            </label>
-            </div>
-
-            <div>
-            <input class="form-check-input" type="checkbox" value="Guinea Pig" id="guineapig" v-model="typeofpet">
-            <label class="form-check-label ps-2" for="guineapig">
-            <img src="../img/animallogo/guineapiglogo.png" style="width:70px; height:60px;">
-                Guinea Pig
-            </label>
-            </div>
-
-            <div>
-            <input class="form-check-input" type="checkbox" value="Birds" id="bird" v-model="typeofpet">
-            <label class="form-check-label ps-2" for="bird">
-            <img src="../img/animallogo/birdlogo.png" style="width:70px; height:60px;">
-                Birds
-            </label>
+                <div class="container py-5" style="text-align: center;"><button @click="toggleModal"
+                        class="btn btn-go my-2">Submit</button></div>
             </div>
             
-            <div>
-            <input class="form-check-input" type="checkbox" id="other" value="other" v-model="typeofpet">
-            <label for="other">&nbsp;Others &nbsp;</label>
-            <input type="text" id="otherValue" placeholder="please specify" />
-            </div>
+            <div class="col-1"></div>
 
-        </div>
+            <div class="col-1 sides"></div>
         
-        <div class="input-row">
-            
-            <div class="input-container">
-            <label for="petTypes">Size of pets you accept</label>
-                <select name="number" id="size" v-model="sizeofpet" style="height:30px;">
-                <option value="1kg">1-5kg</option>
-                <option value="5kg">5-10kg</option>
-                <option value="10kg">10-20kg</option>
-                <option value="20kg">20-40kg</option>
-                <option value="40kg">40+kg</option>
-                </select>
-            </div>
-
-            <div class="input-container">
-                <label  for="yearsofexp">Years of Experience</label>
-                <input type="number" id="yearsofexp" min="0" max="50" >
-            </div>
-
-            <div class="input-container">
-            <label for="rate">What is your rate SGD (per hour)</label>
-                <input type="number" min="0" max="1000" v-model="rate">
-            </div>       
         </div>
 
-        <div class="container" style="text-align: center;"><button @click="toggleModal" class="btn btn-go">Submit</button></div>   
-        </div>
+        
     </div>
 
+
     <Modal @close="toggleModal" :modalActive="modalActive">
-        <h2 style="text-align:center">Submitted!</h2>            
+        <h3 class="text-center">Received</h3>
+        <p class="text-center pt-2">Your listing will be published soon!</p>
     </Modal>
-    
+
     <petpalsFooter></petpalsFooter>
 </template>
     
 <script>
-    import Modal from '../components/Modal.vue'
-    import navbar from '../components/navbar.vue'
-    import { ref } from "vue";
-    import petpalsFooter from "../components/petpalsFooter.vue"
-    
-    export default {
-        name: "profilelisting",
-        components: {
-            Modal, navbar, petpalsFooter, petpalsFooter
-        },
-        data() {
-                return {
-                showModal: false,
-                services: [],
-                description: '',
-                typeofpet: [],
-                sizeofpet:'',
-                rate: 0,
-                }
-            },
-        setup() {
-            const modalActive = ref(false);
-            const toggleModal = () => {
+import Modal from '../components/Modal.vue'
+import navbar from '../components/navbar.vue'
+import { ref } from "vue";
+import petpalsFooter from "../components/petpalsFooter.vue"
+
+export default {
+    name: "profilelisting",
+    components: {
+        Modal, navbar, petpalsFooter, petpalsFooter
+    },
+    data() {
+        return {
+            showModal: false,
+            services: [],
+            description: '',
+            typeofpet: [],
+            sizeofpet: '',
+            location: '',
+            rate: 0,
+            displayName: '',
+            contactNumber: '',
+            email: ''
+        }
+    },
+    setup() {
+        const modalActive = ref(false);
+        const toggleModal = () => {
             modalActive.value = !modalActive.value;
-            };
-            return { modalActive, toggleModal };
-        },
-    };
+        };
+        return { modalActive, toggleModal };
+    },
+};
 </script>
     
 <style scoped>
-    .content {
-      padding: 120px;
-      background:  #fffbfa;
-    }
-    
-    .inner {
-      margin: auto;
-      width: 60%;
-    }
-  
-    .top-div {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      margin-bottom: 50px;
-    }
-    
-    h6 {
-      font-size: 25px;
-      font-weight: 500;
-    }
+.content {}
 
-    label {
-        margin-bottom: 10px;
-    }
-    
-    
-    .input-row {
-      display: flex;
-      justify-content: space-between;
-    }
-    
-    .input-container {
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 30px;
-    }
+.inner {
+    margin: auto;
+    width: 60%;
+}
 
+.top-div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 50px;
+}
+
+h6 {
+    font-size: 25px;
+    font-weight: 500;
+}
+
+label {
+    margin-bottom: 10px;
+}
+
+
+.input-row {
+    display: flex;
+    justify-content: space-between;
+}
+
+.input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 30px;
+}
 </style>
