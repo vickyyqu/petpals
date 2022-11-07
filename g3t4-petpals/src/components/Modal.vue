a76a9623b22ae7d6a138818cf1156259051fbc19
vickyyqu <112297073+vickyyqu@users.noreply.github.com>
2022-11-08 01:04:36 +0800

    added geocoding codes and styling

--- g3t4-petpals/src/components/Modal.vue
+++ g3t4-petpals/src/components/Modal.vue
@@ -15,8 +15,8 @@
     width: 80%;
     box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     background-color: #fff;
-    padding: 64px 16px;
     border-radius:15px;
+    padding: 20px;
   }
 }
 </style>
