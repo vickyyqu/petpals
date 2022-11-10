02d66ba5fa2092ae12dc8429057a1668c69cf9fc
jadatanjq <114057423+jadatanjq@users.noreply.github.com>
2022-11-11 02:07:02 +0800

    -

--- g3t4-petpals/src/components/events.vue
+++ g3t4-petpals/src/components/events.vue
@@ -6,21 +6,18 @@
 <template>
 <div class="card mx-auto " style="width:175px;" >
     <div class="card-body">
-        <div class="row">
-            <div class="col-9">
-                <h4 class="card-reviewID">{{title}}</h4>
-                <h5></h5>
-            </div>
-            <div class = 'col-3'>
-                <span><button class="btn btn-select px-2 py-1 float-end" v-on:click = "$emit('delete')"><i class="bi bi-trash3"></i></button></span>
-            </div>
-        </div>  
+        <h4 class="card-reviewID text-center">{{title}}</h4>
+
         <div class = 'row my-2'>
-            <small class="card-reviewer"> Venue: {{venue}}</small>
+            <small class="card-reviewer">Venue: {{venue}}</small>
+            <div class = 'col-9 pe-0'>
+            <small class="card-reviewer">Starts: {{getTime(start)}}</small>
                 <br/>
-            <small class="card-reviewer"> Starts: {{getTime(start)}}</small>
-                <br/>
-            <small class="card-reviewer"> Ends: {{getTime(end)}}</small>
+            <small class="card-reviewer">Ends: {{getTime(end)}}</small>
+            </div>
+            <div class = 'col-3 px-0 d-flex align-items-center justify-content-start'>
+                <span><button class="btn btn-select px-2 py-1" v-on:click = "$emit('delete')"><i class="bi bi-trash3"></i></button></span>
+            </div>
         </div>
     </div>
 </div>
