a483ca0d07d8c1b05f214062801650443c076c07
jadatanjq <114057423+jadatanjq@users.noreply.github.com>
2022-11-11 01:35:34 +0800

    update calendar.vue and events.vue

--- g3t4-petpals/src/components/events.vue
+++ g3t4-petpals/src/components/events.vue
@@ -4,20 +4,21 @@
 </style>
 
 <template>
-<div class="card mx-auto " style="width:150px;" >
+<div class="card mx-auto " style="width:175px;" >
     <div class="card-body">
         <div class="row">
             <div class="col-9">
                 <h4 class="card-reviewID">{{title}}</h4>
+                <h5></h5>
             </div>
             <div class = 'col-3'>
-                <span><button class="btn btn-select px-3 ms-1 float-end" v-on:click="$emit('edit')"><i class="bi bi-pencil-square"></i></button></span>
+                <span><button class="btn btn-select px-2 py-1 float-end" v-on:click = "$emit('delete')"><i class="bi bi-trash3"></i></button></span>
             </div>
         </div>  
         <div class = 'row my-2'>
-            <small class="card-reviewer"> Starts: </small>
+            <small class="card-reviewer"> Starts: {{getTime(start)}}</small>
                 <br/>
-            <small class="card-reviewer"> Ends: </small>
+            <small class="card-reviewer"> Ends: {{getTime(end)}}</small>
         </div>
     </div>
 </div>
@@ -25,9 +26,22 @@
 
 <script>
 export default {
-    props: ['date','title','from', 'to'], // day: 9, month: 'Nov', year: '2022'
+    props: ['title','start','end'], 
     methods: {
+        getTime(time){
+            var stime = time.split(':')
+            if (Number(stime[0]) > 12){
+                stime[0] = (Number(stime[0])-12).toString()
+                return stime[0] + ':' + stime[1] + ' PM'
 
+            }else if (stime[0] == '00'){
+                stime[0] = '12'
+            }
+
+            if (Number(stime[0]) < 12){
+                return Number(stime[0]) + ':' + stime[1] + ' AM'
+            }
+        }
     },
 }
 </script>
--- g3t4-petpals/src/views/calendar.vue
+++ g3t4-petpals/src/views/calendar.vue
@@ -22,26 +22,27 @@ p {
 }
 
 .body{
-    height: 600px;
+    height: 500px;
     background-color: white;
     border-radius: 20px;
 }
 
 .bookings {
     overflow: scroll;
-    height: 500px;
+    height: 400px;
 }
 
 .event{
     background-color: #F8AA9D;
     color:white;
-    border-radius: 25%;
     padding: 5px;
+    width: 100%;
 }
 
 .days{
     overflow: scroll;
-    height: 125px;
+    height: 55px;
+    width: 100px;
 }
 
 </style>
@@ -51,9 +52,9 @@ p {
     <navbar v-if="petOwner"></navbar>
     <navbarProvider v-else></navbarProvider>
 
-    <div class = 'row mt-5 pt-3 justify-content-center' >
+    <div class = 'row mt-5 pt-3 justify-content-center'>
         <div class="col sides">
-            <div class = 'row mx-2'>
+            <div class = 'row mx-2 mb-5'>
                 <div class="col-9">
                     <div id="carouselExampleControls" class="carousel carousel-dark slide"  data-bs-interval="false" data-bs-wrap="false">
                         <div class="carousel-inner" >
@@ -67,36 +68,36 @@ p {
                                     </div>
                                 </div>
 
-                                <div class = 'row my-1' v-for="week in obj.arr.slice(0,1)">
-                                    <div class = 'row mb-1'>
+                                <div class = 'row mt-1' v-for="week in obj.arr.slice(0,1)">
+                                    <div class = 'row'>
                                         <div class = 'col' style='width:100px;' v-for='n in (7-Object.keys(week).length)'>
                                             <p></p>
                                         </div>
                                         <div class = 'col' style='width:100px;' v-for='n in (Object.keys(week))'>
-                                            <p v-if='today.day==n && months[today.month].toUpperCase()==obj.mth' v-on:click="chosenDate.day = n, chosenDate.month=(today.month+idx)%12">
+                                            <p v-if='today.day==n && months[today.month].toUpperCase()==obj.mth' v-on:click="updateChosen(n,idx)">
                                                 <span class=" btn current">{{n}}</span>
                                             </p>
-                                            <p v-else  v-on:click="chosenDate.day = n, chosenDate.month=(today.month+idx)%12">
+                                            <p v-else  v-on:click="updateChosen(n,idx)">
                                                 <span :class="{current2: n==chosenDate.day && months[chosenDate.month].toUpperCase()==obj.mth}">{{n}}</span></p>
                                         </div>
                                     </div>
-                                    <div class = 'row '>
-                                        <div class = 'col' style='width:100px; height:75px;' v-for='n in (7-Object.keys(week).length)'>
+                                    <div class = 'row'>
+                                        <div class = 'col days' v-for='n in (7-Object.keys(week).length)'>
                                             <p></p>
                                         </div>
-                                        <div class = 'col days' style='width:100px; height:75px;' v-for='n in (Object.keys(week))'>
-                                            <p class="event" v-for="event in week[n]">{{event.title}}</p>
+                                        <div class = 'col days' v-for='n in (Object.keys(week))'>
+                                            <p class="event rounded" v-for="event in week[n]"></p>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class = 'row my-1' v-for="week in obj.arr.slice(1,obj.arr.length)">
-                                    <div class = 'row mb-1'>
+                                <div class = 'row mt-1' v-for="week in obj.arr.slice(1,obj.arr.length)">
+                                    <div class = 'row'>
                                         <div class = 'col' style='width:100px;' v-for='n in (Object.keys(week))'>
-                                            <p v-if='today.day==n && months[today.month].toUpperCase()==obj.mth' v-on:click="chosenDate.day = n, chosenDate.month=(today.month+idx)%12">
+                                            <p v-if='today.day==n && months[today.month].toUpperCase()==obj.mth' v-on:click="updateChosen(n,idx)">
                                                 <span class="current">{{n}}</span> 
                                             </p>
-                                            <p v-else v-on:click="chosenDate.day = n, chosenDate.month=(today.month+idx)%12">
+                                            <p v-else v-on:click="updateChosen(n,idx)">
                                                 <span :class="{current2: n==chosenDate.day && months[chosenDate.month].toUpperCase()==obj.mth}">{{n}}</span></p>
                                         </div>
                                         <div class = 'col' style='width:100px;' v-for='n in (7-Object.keys(week).length)'>
@@ -104,10 +105,10 @@ p {
                                         </div>
                                     </div>
                                     <div class = 'row'>
-                                        <div class = 'col days' style='width:100px; height:75px;' v-for='n in (Object.keys(week))'>
-                                            <p class="event" v-for="event in week[n]">{{event.title}}</p>
+                                        <div class = 'col days' v-for='n in (Object.keys(week))'>
+                                            <p class="event rounded" v-for="event in week[n]"></p>
                                         </div>
-                                        <div class = 'col' style='width:100px; height:75px;' v-for='n in (7-Object.keys(week).length)'>
+                                        <div class = 'col days' v-for='n in (7-Object.keys(week).length)'>
                                            <p></p>
                                         </div>
                                     </div>
@@ -127,21 +128,62 @@ p {
                 </div>
 
                 <div class = 'col-3'>
-                    <button class="btn btn-select px-3 mt-5 mb-3 w-100" v-on:click="">Add Event</button>
-                    <div class="w-100 body" >
+                    <button class="btn btn-select px-3 mt-5 mb-3 w-100" @click="toggleModal">Add Event &nbsp;<i class="bi bi-calendar-check"></i></button>
+                    <div class="body">
                         <h6 class = 'text-center p-2'>{{chosenDate.day}}-{{months[chosenDate.month]}}-{{chosenDate.year}}</h6>
                         <h4 class = 'text-center'>BOOKINGS</h4>
                         <div class="bookings mt-3">
-                            <events v-for="n in 5" :date="chosenDate" :title='"hello"'  class="mt-5"></events>
+                            <events v-for="item in events" :title='item.title' :start="item.start" :end="item.end" v-on:delete="deleteEvent(item)" class="my-2 mb-4"></events>
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
 
-    
+<Modal @close="addEvent()" :modalActive="modalActive">
+  <div class="modal-content p-3" >
+    <h4 class="p-2 my-1" style="background-color: #fddcd74d; border-radius: 4px;">Enter event details:
+        <span class = 'float-end'><button class = 'btn btn-select px-1 py-0' v-on:click="toggleModal(true);"><i class="bi bi-x-lg"></i></button></span>
+    </h4>
+
+    <div v-if="addError" class="text-center mt-3">
+        <small style="font-style:italic; color:brown">{{errorMsg}}</small>
+    </div>
+
+    <label>Event Title:</label>
+    <input class="form-control mb-2"
+      type="text"
+      id="title"
+      v-model="title"
+      required
+    />
+
+    <label>Date:</label>
+    <input class="form-control mb-2"
+      type="date"
+      id="eventDate"
+      v-bind:min="today.year + '-' + (today.month+1) + '-' + today.day"
+      v-model="eventDate"
+      width="50%"
+      required
+    />
+
+    <label>Start:</label>
+    <input class="form-control mb-2"
+      type="time"
+      id="startTime"
+      v-model="startTime"
+    />
+
+    <label>End:</label>
+    <input class="form-control mb-2"
+      type="time"
+      id="endTime"
+      v-model="endTime"
+    />
+  </div>
+</Modal> 
 </div>
 
 <petpalsFooter></petpalsFooter>
@@ -152,9 +194,11 @@ import navbar from '@/components/navbar.vue'
 import navbarProvider from '@/components/navbarProvider.vue'
 import events from '@/components/events.vue'
 import petpalsFooter from "@/components/petpalsFooter.vue"
+import Modal from "@/components/Modal.vue";
+import { ref as modalref } from "vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getDatabase, ref, onValue, set, get } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAS74F4gerXVK8OW-RBq3rSGNEoHuqLQ0A",
@@ -168,6 +212,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const auth = getAuth(); 
 
 export default {
     data(){
@@ -177,6 +222,14 @@ export default {
             chosenDate: {'day': 0, 'month': '', 'year': ''},
             events: [],
             year: [],
+
+            title: '',
+            eventDate: '',
+            startTime: '09:00',
+            endTime: '10:00',
+
+            addError: false,
+            errorMsg: '',
 
             months: ['January', 'Febuary', 'March', 'April', 'May', 'June','July', 
                         'August', 'September', 'October', 'November', 'December'],
@@ -189,11 +242,38 @@ export default {
         navbar,
         navbarProvider,
         events,
+        Modal,
         petpalsFooter,
     },
 
     methods : {
-        getMonth(startDay,totalDays,month,year){
+
+        getYear(){
+            for (let i=this.today.month; i<this.today.month+12; i++){
+                var sday = 0
+                var tdays = 0
+                var tyear = this.today.year
+                var idx = i
+
+                if (i >= 12){
+                    idx -= 12
+                    tyear = this.today.year + 1
+                }
+
+                sday = new Date(`${this.months[idx]} 1, ${tyear} 01:15:00`).getDay();
+                tdays = new Date(tyear, idx+1, 0).getDate()
+                
+
+                var obj = {}
+                obj['year'] = tyear
+                obj['arr'] = this.getMonth(sday,tdays,idx+1,tyear)
+                obj['mth'] = this.months[idx].toUpperCase()
+                this.year.push(obj)
+            
+            }
+        },
+
+        getMonth(startDay,totalDays, month, year){
             var lst = [];
             var count = 1
             var weeks = 4 
@@ -209,7 +289,8 @@ export default {
 
             var obj = {}
             for(let i=0; i<(7-startDay); i++){
-                obj[count] = this.getEvents(count,month,year)
+                var temp = year + '-' + month + '-' + count 
+                obj[count] = this.getEvents(temp)
                 count ++
             }
             lst.push(obj)
@@ -218,7 +299,8 @@ export default {
             for (let i=0; i<weeks-1; i++){
                 obj = {}
                 for (let j=0; j<7; j++){
-                    obj[count] = this.getEvents()
+                    var temp = year + '-' + month + '-' + count 
+                    obj[count] = this.getEvents(temp)
                     count ++
                 }
                 lst.push(obj)
@@ -226,27 +308,118 @@ export default {
 
             obj = {}
             for(let i=count; i<=(totalDays); i++){
-                obj[i] = this.getEvents()
+                var temp = year + '-' + month + '-' + i
+                obj[i] = this.getEvents(temp)
             }
-            lst.push(obj) 
 
+            lst.push(obj) 
             return lst
         },
 
         getEvents(date){
+            // date in 'yyyy-mm-dd' format
             var lst = []
-            var obj = {'title': '     ', 'date': '', 'time': ''}
-            return lst.push(obj)
+
+            var temp = date.split('-')
+            if (temp[1].length == 1){
+                temp[1] = '0'.concat(temp[1])
+            }
+            if (temp[2].length == 1){
+                temp[2] = '0'.concat(temp[2])
+            }
+            date = temp.join('-')
+
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    get(ref(db, `users/${user.uid}/calendar/${date}`))
+                    .then((snapshot) => {
+                        if (snapshot.exists()) {
+                            this.events = []
+                            for (let event in snapshot.val()) {
+                                var obj = {}
+
+                                obj['title'] = event
+                                obj['start'] = snapshot.val()[event].start
+                                obj['end'] = snapshot.val()[event].end
+                                this.events.push(obj)
+                                lst.push(obj)
+                            }
+                            
+                        }else{
+                            this.events = []
+                        }
+                        
+                    })
+                }
+
+            })
+            return lst
+
         },
 
-        addEvent(date){
+        addEvent(){
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    if (this.title != '' && this.startTime <= this.endTime) {
+                        console.log(this.title, this.eventDate, this.startTime, this.endTime)
+                        
+                        set(ref(db,`users/${user.uid}/calendar/${this.eventDate}/${this.title}`), {
+                            'start': this.startTime,
+                            'end': this.endTime
+                        })
 
+                        this.toggleModal(false); 
+
+                    }else{
+                        this.addError = true
+                        if (this.title == ''){
+                            this.errorMsg = 'Please enter a title.'
+                        }else if(this.startTime > this.endTime){
+                            this.errorMsg = 'Start time has to be earlier than end time.'
+                        }
+                    }
+
+                }
+            });
         },
+
+        deleteEvent(item){
+            var temp = this.chosenDate.year + '-' 
+            if ((this.chosenDate.month+1).toString().length == 1){
+                temp += '0' + (this.chosenDate.month+1) + '-'
+            }else{
+                temp += (this.chosenDate.month+1) + '-'
+            }
+            if (this.chosenDate.day.toString().length == 1){
+                temp += '0' + this.chosenDate.day
+            }else{
+                temp += this.chosenDate.day
+            }
+
+            onAuthStateChanged(auth, (user) => {
+                if (user) {
+                    set(ref(db,`users/${user.uid}/calendar/${temp}/${item.title}`), null)
+                    window.location.href = `/calendar`;
+                }
+            });
+        },
+
+        updateChosen(n, idx){
+            this.chosenDate.day = n
+            this.chosenDate.month = (this.today.month+idx)%12
+
+            if (this.today.month + idx < 12){
+                this.chosenDate.year = this.today.year
+            }else{
+                this.chosenDate.year = this.today.year + 1
+            }
+
+            this.getEvents(this.chosenDate.year + '-' + (this.chosenDate.month+1) + '-' + (this.chosenDate.day))
+        }
 
     },
 
     mounted(){
-        const auth = getAuth();
         onAuthStateChanged(auth, (user) => {
             if (user) {
                 onValue(ref(db, `users/${user.uid}`), (snapshot) => {
@@ -264,37 +437,29 @@ export default {
         this.today.month = temp.getMonth()
         this.today.year = temp.getFullYear()
 
-        this.chosenDay = this.today.day
         this.chosenDate.day = this.today.day
         this.chosenDate.month = this.today.month
         this.chosenDate.year = this.today.year
 
-        console.log(this.today)
+        this.eventDate = this.today.year + '-' + (this.today.month+1) + '-' + this.today.day
 
-        for (let i=this.today.month; i<this.today.month+12; i++){
-            var sday = 0
-            var tdays = 0
-            var tyear = this.today.year
-            var idx = i
+        this.getYear()
 
-            if (i >= 12){
-                idx -= 12
-                tyear = this.today.year + 1
+        this.getEvents(this.today.year + '-' + (this.today.month+1) + '-' + (this.today.day))
+
+    },
+
+    setup() {
+        const modalActive = modalref(false);
+
+        const toggleModal = (check) => {
+            modalActive.value = !modalActive.value;
+            if (modalActive.value == modalref(false).value && check == false){
+                window.location.href = `/calendar`;
             }
+        };
 
-            sday = new Date(`${this.months[idx]} 1, ${tyear} 01:15:00`).getDay();
-            tdays = new Date(tyear, idx+1, 0).getDate()
-            
-
-            var obj = {}
-            obj['year'] = tyear
-            obj['arr'] = this.getMonth(sday,tdays)
-            obj['mth'] = this.months[idx].toUpperCase()
-            this.year.push(obj)
-           
-        }
-
-        // console.log(this.year)
+        return { modalActive, toggleModal };
     },
 }
 </script>
